38c3798da56c94d2ffaffc9471846487fbaea8c2
Tô Ngọc Huy <huyngocto12356@gmail.com>
2026-06-06 17:34:00 +0700

    Reapply "Fix backend startup configuration"
    This reverts commit 32c6f514684ff3722c250cd2bccfcfbc65ac6743.

---
+++ backend/local-db.example.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Copy this file to local-db.bat and change the values for your SQL Server.
+set "DB_URL=jdbc:sqlserver://localhost:1433;databaseName=QuanLyThuVien;encrypt=true;trustServerCertificate=true"
+set "DB_USERNAME=sa"
+set "DB_PASSWORD=your_sql_server_password"
