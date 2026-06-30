f93c9f1011e53a637d5fdcc5283f321d17630d42
Le Tuan Duong <combaymau@gmail.com>
2026-06-30 15:25:14 +0700

    Update PhieuMuonRepository

--- backend/src/main/java/com/library/backend/repository/PhieuMuonRepository.java
+++ backend/src/main/java/com/library/backend/repository/PhieuMuonRepository.java
@@ -3,5 +3,8 @@ package com.library.backend.repository;
 import com.library.backend.entity.PhieuMuon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PhieuMuonRepository extends JpaRepository<PhieuMuon, String> {
+    Optional<PhieuMuon> findByIdempotencyKey(String idempotencyKey);
 }
