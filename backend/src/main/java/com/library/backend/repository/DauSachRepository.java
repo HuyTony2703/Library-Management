1c888d56571ae03dc12832499db5d6aa189c08a4
Le Tuan Duong <combaymau@gmail.com>
2026-06-30 15:25:12 +0700

    Update DauSachRepository

--- backend/src/main/java/com/library/backend/repository/DauSachRepository.java
+++ backend/src/main/java/com/library/backend/repository/DauSachRepository.java
@@ -2,10 +2,16 @@ package com.library.backend.repository;
 
 import com.library.backend.entity.DauSach;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface DauSachRepository extends JpaRepository<DauSach, String> {
 
-    boolean existsByIsbn(String isbn);
-
-    boolean existsByIsbnAndMaDauSachNot(String isbn, String maDauSach);
+    @Query(value = """
+            SELECT TOP 1 * FROM DAUSACH
+            WHERE REPLACE(REPLACE(UPPER(ISBN), '-', ''), ' ', '') = :isbn
+            """, nativeQuery = true)
+    Optional<DauSach> findByNormalizedIsbn(@Param("isbn") String isbn);
 }
